389cbe5a484da12b7e6a11d2b0b0320d8ce6e9f8
grantP820 <grant.porter@ascte.org>
2026-09-22 13:40:13 -0500

    Added new song

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("magots for brains", "Oliva rodregeo")
 ]
 
 def print_playlist(songs):
